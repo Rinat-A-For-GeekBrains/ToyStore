16543423e7f7b2a00335131a2b191fbe171b0a03
unknown <alimbekovrl@gmail.com>
2024-02-02 13:18:56 +0500

    Initial commit

---
+++ Second_intermediate_task_ToyStore/src/Shop.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Shop {
+    public PriorityQueue <Toy> toy;
+
+    public Shop(){
+        toy = new PriorityQueue<>(Comparator.comparing())
+    }
+}
---
+++ Second_intermediate_task_ToyStore/src/Toy.java
@@ -0,0 +1,53 @@
+public abstract class Toy {
+    private int id;
+    private String name;
+    private int quantity;
+    private int periodicity;
+
+    public Toy(int id, String name, int quantity, int periodicity) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.periodicity = periodicity;
+    }
+
+    public void setPeriodicity(int periodicity) {
+        this.periodicity = periodicity;
+    }
+
+    public int getPeriodicity() {
+        return periodicity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
+Int x = random.next(101)
+String res=“ “;
+J=0;
+For(I=0; I < вес[].length;I++){
+If(x< вес[i] * 10 + j  return id [i]
+        Break;
+}
+
+J = вес[i] * 10;
